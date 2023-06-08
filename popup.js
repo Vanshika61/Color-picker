0f452688a13660642bd211bb547c1d1a6666f88a
Vanshika <agrawal10614@gmail.com>
2023-06-08 17:49:27 +0530

    adding some changes

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ const colorValue = document.querySelector('.colorValue');
 btn.addEventListener('click', async ()=> {
     chrome.storage.sync.get('color', ({color}) =>{
         console.log('color: ', color);
-    })
+    });
     let[tab] = await chrome.tabs.query({active: true, currentWindow: true});
     
     chrome.scripting.executeScript({
